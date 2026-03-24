28857e81b788b2185da63ff8aec42d12c99e815b
DJ <tron1981@gmail.com>
2026-03-24 23:34:11 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // GitHub Pages 배포를 위한 경로 설정 (레포지토리 이름인 oamkase를 넣어야 합니다)
+  base: "/oamkase/",
+
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +15,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
